8afe5cd7cde4a4906e06f113152405ebe5027dcc
ansur <ansur@LAPTOP-H4Q5TFNK>
2024-02-19 19:12:01 +0530

    Sharing the code first time

---
+++ Javaproject/src/Program/Class1.java
@@ -0,0 +1,11 @@
+package Program;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("kajol katkar");
+	}
+	
+
+}
---
+++ Javaproject/src/Program/Iframe.java
@@ -0,0 +1,10 @@
+package Program;
+
+public class Iframe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
